d737b8244798c73a5a589e014ab51d2b02122794
eya <guesmieya55@gmail.com>
2025-02-19 10:52:36 +0100

    initial commit

---
+++ frontend/MapFlow/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  imports: [CommonModule, ReactiveFormsModule] // ✅ Import required modules
+
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Login Data:', loginData);
+      // Implement login functionality here, e.g., calling a login API
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
